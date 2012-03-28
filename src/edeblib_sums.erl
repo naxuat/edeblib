22105ab84cb200fed07f2ca4b61522581c80332f
Mikhail Sobolev <mss@mawhrin.net>
2012-03-28 19:14:03 +0300

    produce sha256 digest for files

--- src/edeblib_sums.erl
+++ src/edeblib_sums.erl
@@ -11,23 +11,28 @@
 -record(edeblib_sums, {
     size :: integer(),
     md5 :: term(),
-    sha1 :: term()
+    sha1 :: term(),
+    sha256 :: term()
 }).
 
 new() ->
-    #edeblib_sums{size=0, md5=crypto:md5_init(), sha1=crypto:sha_init()}.
+    #edeblib_sums{size=0, md5=crypto:md5_init(), sha1=crypto:sha_init(), sha256=erlsha2:sha256_init()}.
 
 new(Data) when is_binary(Data) ->
     update(new(), Data).
 
-update(#edeblib_sums{size=Size, md5=MD5, sha1=SHA1}, Data) when is_binary(Data) ->
-    #edeblib_sums{size=Size+byte_size(Data), md5=crypto:md5_update(MD5, Data), sha1=crypto:sha_update(SHA1, Data)}.
+update(#edeblib_sums{size=Size, md5=MD5, sha1=SHA1, sha256=SHA256}, Data) when is_binary(Data) ->
+    #edeblib_sums{size=Size+byte_size(Data),
+                  md5=crypto:md5_update(MD5, Data),
+                  sha1=crypto:sha_update(SHA1, Data),
+                  sha256=erlsha2:sha256_update(SHA256, Data)}.
 
 final(#edeblib_sums{}=Context, Data) ->
     final(update(Context, Data)).
 
-final(#edeblib_sums{size=Size, md5=MD5, sha1=SHA1}) -> [
+final(#edeblib_sums{size=Size, md5=MD5, sha1=SHA1, sha256=SHA256}) -> [
     {md5, crypto:md5_final(MD5)},
     {sha1, crypto:sha_final(SHA1)},
+    {sha256, erlsha2:sha256_final(SHA256)},
     {size, Size}
 ].
